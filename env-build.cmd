99ec6a4d771de568cbc84c37ffe070333eebdece
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-18 15:54:58 +0900

    yarn add some modules

--- env-build.cmd
+++ env-build.cmd
@@ -1,1 +1,1 @@
-call yarn add --dev react react-dom react-router material-ui react-tap-event-plugin babel-preset-es2015 babel-preset-react babelify browserify gulp run-sequence gulp-rename gulp-plumber gulp-uglify material-ui react-tap-event-plugin vinyl-source-stream
+call yarn add --dev react react-dom react-router material-ui react-tap-event-plugin babel-preset-es2015 babel-preset-react babelify browserify gulp run-sequence gulp-rename gulp-plumber gulp-uglify material-ui react-tap-event-plugin vinyl-source-stream minimatch@^3 graceful-fs@^4
